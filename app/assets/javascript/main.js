b449d72f6e09799b0346fad5a39417981526e0e6
Tosin <71393402+Tosin-Balogun@users.noreply.github.com>
2021-12-22 12:50:28 +0000

    Update review summary page

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,1 +1,16 @@
 // ES6 or Vanilla JavaScript
+function selectAll(event) {
+    const scope = event.currentTarget.closest("fieldset");
+    const inputs = scope.querySelectorAll("input[type=checkbox]");
+    for(let input of inputs) {
+      input.checked = event.currentTarget.checked;
+    }
+  }
+  
+  const selectAllBoxes = document.querySelectorAll("[data-select-all]");
+  
+  if (selectAllBoxes.length > 0){
+    for (let box of selectAllBoxes) {
+      box.addEventListener("click", selectAll); 
+    }
+  }
