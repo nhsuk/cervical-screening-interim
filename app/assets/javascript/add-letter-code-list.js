b399652037302a3bbe5e18ac2a974b718e23d5a1
Rebecca Gorton <rebecca.gorton1@nhs.net>
2022-01-14 17:22:16 +0000

    Autocomplete

--- app/assets/javascript/add-letter-code-list.js
+++ app/assets/javascript/add-letter-code-list.js
@@ -6,7 +6,6 @@
 //Define empty array where NHS numbers will live when entered
 var letterCodes = []
 //Define variables and helps the html talk to the javascript
-var letterCodeinput = document.getElementById('letter-code-input')
 var letterCodelist = document.getElementById('letter-code-list')
 var letterCodeButton = document.getElementById('add-letter-code')
 
@@ -15,10 +14,12 @@ letterCodeButton.addEventListener('click', addNumberTolist);
 
 //Function to save the nhs number and spit it back out above
 function addNumberTolist() {
-  var number = letterCodeinput.value; // get value of whats inside the box
-  var letterCodelength = letterCodes.push(number) //adds it to the array + adds to new variable
-  letterCodelist.innerHTML+="<li id='letter-code-"+letterCodelength+"'name='letter-code-data' value='{{ data['letter-code-data'] }}'><strong>"+number+"</strong> <span class='nhsuk-body-s' style='display: inline; text-decoration: underline; margin-left:6px'><a class='remove-link' href='javascript:removeNumberFromList("+letterCodelength+")'> Remove </a> </span> </li>" //spit it back out (with some added extras)
-  letterCodeinput.value="" // clears the box for the next input
+  if(document.getElementById('autocomplete-customTemplates').value != "") {
+    var number = document.getElementById('autocomplete-customTemplates').value; // get value of whats inside the box
+    var letterCodelength = letterCodes.push(number) //adds it to the array + adds to new variable
+    letterCodelist.innerHTML+="<li id='letter-code-"+letterCodelength+"'name='letter-code-data' value='{{ data['letter-code-data'] }}'><strong>"+number+"</strong> <span class='nhsuk-body-s' style='display: inline; text-decoration: underline; margin-left:6px'><a class='remove-link' href='javascript:removeNumberFromList("+letterCodelength+")'> Remove </a> </span> </li>" //spit it back out (with some added extras)
+    document.getElementById('autocomplete-customTemplates').value="" // clears the box for the next input
+  }
   
 }
 
@@ -27,5 +28,10 @@ function  removeNumberFromList(id) {
   letterCodeElement.parentNode.removeChild(letterCodeElement);
 }
 
+// when seleting from the list, the value chosen is placed into a hidden input element programatically
+// added to the DOM by the code running the accessible auto complete. This means that trying to get that element by ID at page load doesn't work because
+// it doesn't exist until the user has selected a value.
+// therefore, in the "addNumberToList" function we need to dynamically get the input element
+// after it has been created, in order to get its value.
 
 
