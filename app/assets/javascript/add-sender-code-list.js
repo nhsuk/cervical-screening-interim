698c54f1d7ea1924d7ff811407532ae029e9c323
Tosin <71393402+Tosin-Balogun@users.noreply.github.com>
2022-02-01 16:51:29 +0000

    autcomplete enhancement

--- app/assets/javascript/add-sender-code-list.js
+++ app/assets/javascript/add-sender-code-list.js
@@ -7,7 +7,6 @@
 //Define empty array where NHS numbers will live when entered
 var senderCodes = []
 //Define variables and helps the html talk to the javascript
-var senderCodeinput = document.getElementById('sender-code-input')
 var senderCodelist = document.getElementById('sender-code-list')
 var senderCodeButton = document.getElementById('add-sender-code')
 
@@ -16,6 +15,7 @@ senderCodeButton.addEventListener('click', addNumberTolist);
 
 //Function to save the nhs number and spit it back out above
 function addNumberTolist() {
+  var senderCodeinput = document.getElementById('sender-code-input-autocomplete')
   var number = senderCodeinput.value; // get value of whats inside the box
   var senderCodelength = senderCodes.push(number) //adds it to the array
   senderCodelist.innerHTML+="<li id='sender-code-"+senderCodelength+"'><strong>"+number+"</strong> <span class='nhsuk-body-s' style='display: inline; text-decoration: underline; margin-left:6px'><a class='remove-link' href='javascript:removeNumberFromList("+senderCodelength+")'> Remove </a> </span> </li>" //spit it back out (with some added extras)
